206f7c7c458fb3f5565da2d683475ea81afff355
Chaim B. Sommers <cb@compute.co.il>
2020-06-28 14:24:23 +0300

    Filled out functionality.

---
+++ ZmaniosClock/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ZmaniosClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(615, 148);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.DarkGray;
+            this.button1.Location = new System.Drawing.Point(585, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "x";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Quartz", 82F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(615, 148);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.ForeColor = System.Drawing.Color.LightSalmon;
+            this.richTextBox1.Location = new System.Drawing.Point(43, 15);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(511, 195);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 204);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(591, 227);
+            this.panel2.TabIndex = 2;
+            this.panel2.Visible = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel3.Font = new System.Drawing.Font("Quartz", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panel3.ForeColor = System.Drawing.Color.Lime;
+            this.panel3.Location = new System.Drawing.Point(232, 151);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(150, 32);
+            this.panel3.TabIndex = 3;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.DarkGray;
+            this.button2.Location = new System.Drawing.Point(575, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "...";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(615, 202);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zmanios Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
--- ZmaniosClock/Form1.cs
+++ ZmaniosClock/Form1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 using JewishCalendar;
 
@@ -9,6 +12,8 @@ namespace ZmaniosClock
     {
         private readonly Location _location;
         private DateTime _today = DateTime.Now;
+        private DateTime _now;
+        private JewishDate _jdate;
         private int _netzSeconds, _shkiaSeconds;
         private double _zmaniosDaytimeSecondsPerHour,
                     _zmaniosDaytimeSecondsPerMinute,
@@ -17,24 +22,64 @@ namespace ZmaniosClock
                     _zmaniosNighttimeSecondsPerMinute,
                     _zmaniosNighttimeSecondsPerSecond;
 
-
+        //Double buffers everything
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                // Turn on WS_EX_COMPOSITED
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }        
         public Form1()
         {
             InitializeComponent();
-            _location = new Location("Modiin Illit", 2, 31.932622, -35.042898) { 
-                Elevation = 340, IsInIsrael = true };
+            SetDoubleBuffered(this.panel1);
+            SetDoubleBuffered(this.panel2);
+            SetDoubleBuffered(this.panel3);
+            this._now = this._today;
+            _location = new Location("Modiin Illit", 2, 31.932622, -35.042898)
+            {
+                Elevation = 340,
+                IsInIsrael = true
+            };
         }
 
+        
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.SetZmanim();
+            this.timer2.Start();
+            this.SetZmanim();            
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(this.panel2.Visible)
+            {
+                this.panel2.Visible = false;
+                this.Height -= (panel2.Height + 20);
+            }
+            else
+            {
+                this.panel2.Visible = true;
+                this.Height += (panel2.Height + 20);
+            }
+        }
+
 
         private void SetZmanim()
 
         {
             this.timer1.Stop();
 
+            this._jdate = new JewishDate(this._today);
             var ns = Zmanim.GetNetzShkia(DateTime.Now, this._location);
             this._netzSeconds = ns[0].TotalSeconds;
             this._shkiaSeconds = ns[1].TotalSeconds;
@@ -50,9 +95,19 @@ namespace ZmaniosClock
             this._zmaniosNighttimeSecondsPerSecond = totalNightSeconds / 43200d;
 
 
-            var now = DateTime.Now.TimeOfDay.TotalSeconds;
+            var nowSeconds = DateTime.Now.TimeOfDay.TotalSeconds;
 
-            if (this._netzSeconds <= now && this._shkiaSeconds > now)
+            this.richTextBox1.Clear();
+
+            this.richTextBox1.Text += $@"{this._jdate.ToLongDateString()}
+{this._today.ToLongDateString()}
+Hanetz Hachama: {ns[0].ToString24H(true)}
+Shkias Hachama: {ns[1].ToString24H(true)}
+Hour Zmanios Day: {this._zmaniosDaytimeSecondsPerHour / 60:N2} minutes
+Hour Zmanios Night: {this._zmaniosNighttimeSecondsPerHour / 60:N2}  minutes";
+
+
+            if (this._netzSeconds <= nowSeconds && this._shkiaSeconds > nowSeconds)
             {
                 //DayTime
                 this.timer1.Interval = Convert.ToInt32(this._zmaniosDaytimeSecondsPerSecond * 1000);
@@ -67,15 +122,16 @@ namespace ZmaniosClock
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            this.SuspendLayout();
+            
             double hour, minute, second;
-            var dt = DateTime.Now;
-            if (dt.Day != this._today.Day)
+            this._now = DateTime.Now;
+            if (this._now.Day != this._today.Day)
             {
-                this._today = dt;
+                this._today = this._now;
                 this.SetZmanim();
             }
-            var nowSeconds = dt.TimeOfDay.TotalSeconds;
+
+            var nowSeconds = this._now.TimeOfDay.TotalSeconds;
 
             if (this._netzSeconds <= nowSeconds && this._shkiaSeconds > nowSeconds)
             {
@@ -104,8 +160,32 @@ namespace ZmaniosClock
                 second = (sinceShkia % this._zmaniosNighttimeSecondsPerMinute) /
                     this._zmaniosNighttimeSecondsPerSecond;
             }
-            this.label1.Text = $"{(int)hour:D2}:{(int)minute:D2}:{(int)second:D2}";
-            this.ResumeLayout();
+            this.label1.Text = $"{(int)hour:D2}:{(int)minute:D2}:{(int)second:D2}";            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.panel3.Invalidate();
+        }
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawString(this._now.ToString("HH:mm:ss"), this.panel3.Font, Brushes.Lime, 0, 0);
+        }
+
+        private static void SetDoubleBuffered(Control c)
+        {
+            //Taxes: Remote Desktop Connection and painting
+            //http://blogs.msdn.com/oldnewthing/archive/2006/01/03/508694.aspx
+            if (SystemInformation.TerminalServerSession)
+                return;
+
+            PropertyInfo aProp =
+                  typeof(Control).GetProperty(
+                        "DoubleBuffered",
+                        BindingFlags.NonPublic |
+                        BindingFlags.Instance);
+
+            aProp.SetValue(c, true, null);
         }
 
     }
